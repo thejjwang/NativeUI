16089e9d2cd4868eb7f12564096a0efd63d12478
thejjwang <jonathanjacobwang@gmail.com>
2024-03-19 19:56:54 -0700

    did screenheaderbtn

--- app/index.js
+++ app/index.js
@@ -12,8 +12,33 @@ const Home = () => {
             <Stack.Screen 
                 options={{
                     headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    HeaderTitle: ""
                 }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome 
+
+                    />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                    
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
