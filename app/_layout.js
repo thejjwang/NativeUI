1b14cd9b063bf1fbdd009a47572bbf8024e779eb
thejjwang <jonathanjacobwang@gmail.com>
2024-03-20 11:22:41 -0700

    added Dmfonts

--- app/_layout.js
+++ app/_layout.js
@@ -6,6 +6,13 @@ import * as SplashScreen from 'expo-splash-screen';
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    });
+
+
     return <Stack />
 }
 
