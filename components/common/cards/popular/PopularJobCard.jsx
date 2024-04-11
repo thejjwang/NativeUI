45b2fd369dee39ab2fc034cddb6939391deb217f
thejjwang <jonathanjacobwang@gmail.com>
2024-04-11 11:54:40 -0700

    added job containers and logos

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -8,7 +8,13 @@ const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
       style={styles.container(selectedJob, item)}
       onPress={() => handlePressItem(item)}
     >
-
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image 
+          source={{ uri: item.employer_logo }}
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
     </TouchableOpacity>
   )
 }
